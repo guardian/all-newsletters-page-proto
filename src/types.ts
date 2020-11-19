86ce5ef80f072c2cb40b55c810cb11995a71afb2
Josh Buckland <josh.russell.buckland@gmail.com>
2020-11-19 13:31:50 +0000

    Add basic data types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
-export interface Example {
-	name: string;
+export interface Newsletter {
+	listName: string;
+	title: string;
+	description: string;
+	frequency: string;
+	imageUrl?: string;
+}
+
+export interface User {
+	loggedIn: boolean;
+	email?: string;
 }
