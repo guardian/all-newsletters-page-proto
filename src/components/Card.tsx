3dc44d18c668048d1e24cd38a9fa63400ed4d98e
Josh Buckland <josh.russell.buckland@gmail.com>
2020-11-19 15:58:58 +0000

    Implement styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { Button } from '@guardian/src-button';
+import { body, headline } from '@guardian/src-foundations/typography';
+import { TextInput } from '@guardian/src-text-input';
+import React, { useState } from 'react';
 import type { Newsletter, User } from '../types';
 
 export interface CardProps {
@@ -15,12 +21,25 @@ interface NewsletterSignUpProps {
 	user: User;
 }
 
+const cardStyles = css`
+	width: 220px;
+	border-width: 2px;
+`;
+
+const titleStyle = css`
+	${headline.xxsmall()};
+`;
+
+const descriptionStyle = css`
+	${body.small()};
+`;
+
 const NewsletterInfo: React.FC<NewsletterInfoProps> = ({ newsletter }) => {
 	const { title, description } = newsletter;
 	return (
 		<div>
-			<div>{title}</div>
-			<div>{description}</div>
+			<div css={titleStyle}>{title}</div>
+			<div css={descriptionStyle}>{description}</div>
 		</div>
 	);
 };
@@ -29,28 +48,34 @@ const NewsletterSignUp: React.FC<NewsletterSignUpProps> = ({
 	newsletter,
 	user,
 }) => {
+	const [userEmail, setUserEmail] = useState(user.loggedIn ? user.email : '');
+	const [isValidEmail, setIsValidEmail] = useState(true);
 	const emailField = user.loggedIn ? (
-		<input
-			type="email"
-			name="email"
-			placeholder="Email address"
-			value={user.email}
+		<TextInput
+			label="Enter your email address:"
+			supporting="Your email has been pre-populated from your account"
+			width={30}
+			value={userEmail}
+			error={isValidEmail ? 'Please enter a valid email address' : ''}
 			required
 		/>
 	) : (
-		<input type="email" name="email" placeholder="Email address" required />
+		<TextInput
+			type="email"
+			label="Enter your email address:"
+			value={userEmail}
+			onChange={(event) => setUserEmail(event.target.value)}
+			width={30}
+			required
+		/>
 	);
 	return (
 		<form action="https://www.theguardian.com/email" method="post">
 			{emailField}
 			<input type="hidden" name="listName" value={newsletter.listName} />
-			<button
-				data-link-name={`Subscribe to ${newsletter.listName}`}
-				type="button"
-				value="37"
-			>
-				<span>Sign up</span>
-			</button>
+			<Button data-link-name={`Subscribe to ${newsletter.listName}`}>
+				Sign up
+			</Button>
 		</form>
 	);
 };
@@ -59,7 +84,7 @@ export const Card: React.FC<CardProps> = ({
 	newsletter,
 	user = { loggedIn: false },
 }) => (
-	<div>
+	<div css={cardStyles}>
 		<NewsletterInfo newsletter={newsletter} />
 		<NewsletterSignUp newsletter={newsletter} user={user} />
 	</div>
